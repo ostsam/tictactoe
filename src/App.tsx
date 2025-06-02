4a519070d8da09f9900fa4d5d2c5fbe62cc2c964
ostsam <ost.sam@gmail.com>
2025-06-02 17:55:23 -0400

    Complete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,59 @@
-// import { useState } from 'react'
+import { useState } from "react";
 import "./App.css";
 import "./logic/logic.ts";
-import { initBoard } from "./logic/logic.ts";
+import { initGame, move } from "./logic/logic.ts";
 
 function App() {
-  // const [board, setBoard] = useState([
+  const [game, setGame] = useState(initGame());
 
-  const newBoard = initBoard();
+  const click = (rowIdx: number, colIdx: number) => {
+    setGame(move(game, rowIdx, colIdx));
+  };
 
-  return <div>{JSON.stringify(newBoard)}</div>;
+  ///let newBoard = initGame();
+  const boardHtml = (
+    <div className="board">
+      {game.board.map((row, rowIdx) => {
+        return (
+          <div key={rowIdx} className="flex">
+            {row.map((col, colIdx) => {
+              return (
+                <div
+                  key={colIdx}
+                  className="col"
+                  onClick={() => click(rowIdx, colIdx)}
+                >
+                  {JSON.stringify(col)}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+
+  const displayWinner = () => {
+    if (game.endState == "X") {
+      return "X won!";
+    } else if (game.endState == "O") {
+      return "O won!";
+    } else return "Tie";
+  };
+
+  return (
+    <>
+      <div className="h1">Current Player: {game.currentPlayer}</div>
+      {game.endState ? <div>{displayWinner()}</div> : boardHtml}
+      <button
+        onClick={() => {
+          setGame(initGame());
+        }}
+      >
+        New Game
+      </button>
+    </>
+  );
 }
 
 export default App;
--- src/logic/logic.ts
+++ src/logic/logic.ts
@@ -1,22 +1,102 @@
 export type Cell = Player | null;
-export type Board = [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell];
+export type Board = [
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell]
+];
 export type Player = "X" | "O";
+
+export type EndState = "X" | "O" | "tie" | undefined;
 
 export type Game = {
   board: Board;
   currentPlayer: Player;
+  endState?: EndState;
 };
 
-export const initBoard = (): Game => {
+//type WinState = Player | "tie";
+
+export const initGame = (): Game => {
   return {
-    board: [null, null, null, null, "X", null, null, null, null],
+    board: [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ],
     currentPlayer: "X",
   };
 };
 
-export default function move(game: Game, cellIdx: number): Game {
+const winStates = [
+  [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ],
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+  ],
+  [
+    [0, 1],
+    [1, 1],
+    [2, 1],
+  ],
+  [
+    [0, 2],
+    [1, 2],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ],
+  [
+    [0, 2],
+    [1, 1],
+    [2, 0],
+  ],
+];
+
+const getWinner = (game: Game, player: Player) => {
+  return winStates.some((winState) => {
+    return winState.every((rowIndexPair) => {
+      return game.board[rowIndexPair[0]][rowIndexPair[1]] == player;
+    });
+  });
+};
+
+const xWins = (game: Game) => getWinner(game, "X");
+const oWins = (game: Game) => getWinner(game, "O");
+
+function calculateEndState(game: Game): EndState {
+  if (
+    game.board.every((row) => {
+      return row.every((cell) => cell !== null);
+    })
+  )
+    return "tie";
+  if (xWins(game)) return "X";
+  if (oWins(game)) return "O";
+
+  return undefined;
+}
+
+export function move(game: Game, rowIdx: number, colIdx: number): Game {
   const newGame = structuredClone(game);
-  if (newGame.board[cellIdx] != null) {
+  if (newGame.board[rowIdx][colIdx] != null) {
     console.log("Occupied square"); ///Error message in UI later
     return newGame;
   }
@@ -25,16 +105,8 @@ export default function move(game: Game, cellIdx: number): Game {
   } else {
     newGame.currentPlayer = "X";
   }
-  newGame.board[cellIdx] = game.currentPlayer;
-
+  newGame.board[rowIdx][colIdx] = game.currentPlayer;
+  newGame.endState = calculateEndState(newGame);
+  console.log(newGame.endState);
   return newGame;
-
-  // 1. Pick a cell
-  //arg within function
-  // 2. Occupying a cell
-  // 3. Is the cell occupied? y/n
-  // 4. If not occupied, then fill it
-  // 5. Else error
-  // 4. Switch player
-  // 5. Determine winner/tie
 }
