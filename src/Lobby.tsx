a7f141f5730800773bba33e4a0e9fad535f625c8
ostsam <ost.sam@gmail.com>
2025-06-05 18:47:29 -0400

    Added UI to lobby

--- src/Lobby.tsx
+++ src/Lobby.tsx
@@ -5,18 +5,28 @@ import { useState } from "react";
 export default function Lobby() {
   const { games: initialGames } = useLoaderData<{ games: Game[] }>();
 
-  console.log(initialGames);
   const [games, setGames] = useState<Game[]>(initialGames);
 
   return (
     <div>
-      <h1>Let's all go to the lobby and get ourselves a game</h1>
-      {games.map((game) => (
-        <div key={game.id}>
-          {" "}
-          <Link to={`/game/${game.id}`}>{game.id}</Link>
+      <div className="max-w min-h-screen flex flex-col items-center bg-slate-900 p-6">
+        <div className="max-w-md bg-slate-800 rounded-xl shadow-lg overflow-hidden">
+          <div className="bg-gradient-to-r from-slate-700 to-slate-800 px-6 py-4">
+            <h1 className="text-center my-10 text-4xl font-bold text-slate-100 tracking-wide">
+              It's Lobbyin' Time:
+            </h1>
+            {games.map((game) => (
+              <div
+                key={game.id}
+                className="flex flex-row justify-center mt-4 w-full px-4 py-2 bg-green-700 text-slate-100 font-medium rounded-md shadow-md hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition"
+              >
+                {" "}
+                <Link to={`/game/${game.id}`}>{game.id}</Link>
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
