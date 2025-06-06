af3a99c7d3b3b4da2047918164f6b54f70197a04
ostsam <ost.sam@gmail.com>
2025-06-06 19:37:41 -0400

    whoops lol

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 const GAME_UPDATED = "game-updated";
 const USER_JOINED = "user-joined";
-const SERVER_URL = "hhttps://tictactoe-319a.onrender.com";
+const SERVER_URL = "https://tictactoe-319a.onrender.com";
 const CLIENT_URL = "https://tictactoe-lime-sigma.vercel.app";
 
 export { GAME_UPDATED, USER_JOINED, SERVER_URL, CLIENT_URL };
