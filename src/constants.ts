e76f07e94a439c8410c951da31f42f6a0b04b58b
ostsam <ost.sam@gmail.com>
2025-06-06 17:27:48 -0400

    Dockerfile typo

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
 const GAME_UPDATED = "game-updated";
 const USER_JOINED = "user-joined";
+const SERVER_URL = "https://tictactoe-pvxh.onrender.com";
 
-export { GAME_UPDATED, USER_JOINED };
+
+export { GAME_UPDATED, USER_JOINED, SERVER_URL,  };
